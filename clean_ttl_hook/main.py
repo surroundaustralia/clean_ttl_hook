8056fc1679c2c53eef21aba26930ad4f4a75c7f4
adamdavis2 <adam.davis@surroundaustralia.com>
2022-04-19 14:28:36 +1000

    test pre-commit hook

--- clean_ttl_hook/main.py
+++ clean_ttl_hook/main.py
@@ -4,7 +4,26 @@ from pathlib import Path
 import rdflib
 from rdflib import Graph
 
+
 def clean_ttl(input_file_path:Path):
+    #get a list of all leading comments in the file
+    comments_list = []
+    comment_flag = False
+    with open(input_file_path, "r") as f:
+        for index, line in enumerate(f):
+            if len(line.strip()) > 0 and line.strip()[0] == '#' and index == 0:
+                comments_list.append(line)
+                comment_flag = True
+
+            elif len(line.strip()) > 0 and line.strip()[0] == '#' and comment_flag:
+                comments_list.append(line)
+
+            elif len(line.strip()) > 0 and line.strip()[0] != '#':
+                comment_flag = False
+
+            elif not comment_flag:
+                break
+
     g = Graph()
     g.parse(input_file_path)
     all_ns = [n for n in g.namespaces()]
@@ -27,6 +46,14 @@ def clean_ttl(input_file_path:Path):
     for s, p, o in g:
         f.add((s, p, o))
     f.serialize(destination=input_file_path, format='turtle')
+
+    with open(input_file_path, "r") as f:
+        lines = f.readlines()
+
+    comments_list.extend(lines)
+    with open(input_file_path, "w") as f:
+        comments_list = "".join(comments_list)
+        f.write(comments_list)
 
 def main():
     parser = argparse.ArgumentParser()
